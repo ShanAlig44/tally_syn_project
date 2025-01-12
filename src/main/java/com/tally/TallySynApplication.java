29c8c283c957f7c610edecd9dd0bd84c34581947
Arbaj Ahmad <98795559+abjahmad@users.noreply.github.com>
2025-01-12 15:47:07 +0530

    first commit by Arbaj

---
+++ src/main/java/com/tally/TallySynApplication.java
@@ -0,0 +1,13 @@
+package com.tally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallySynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallySynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tally_syn/TallySynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally_syn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallySynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
